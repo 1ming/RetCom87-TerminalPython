18dadfd8695e24a9b3fe08ea560bcf6bd617350b
Ming Zhang <w98zhang@gmail.com>
2024-02-28 16:05:17 -0800

    finished implementing memory dump function. added dump for the monitor rom.

--- dumpmem.py
+++ dumpmem.py
@@ -3,28 +3,28 @@
 """Script for dumping contents of memory into a file."""
 
 import time
-
-import pexpect
 from pexpect import fdpexpect
 
 from rcterm import RetCom87
 
-MAX_READ_BYTES = 256
 
+# get memory dump within a range of addresses
+def memory_dump(device, low, high, save_file="dump.txt", verbose=True):
+    low_pair = [a.encode() for a in low.split(":")]
+    high_pair = [a.encode() for a in high.split(":")]
 
-def request_data():
-    low = "00:0200"
-    high = "00:0300"
+    with fdpexpect.fdspawn(device) as reader:
+        # request a memory dump and provide the low and high addresses
+        reader.send(b"d")
+        for first, second in (low_pair, high_pair):
+            reader.expect(b"BB:AAAA")
+            reader.send(first)
+            reader.expect(b":")
+            reader.send(second)
+        time.sleep(1)
 
-    # request memory dump `d`
-    rc.write_message(b"d")
-    rc.read_until("BB:AAAA")
-
-    # send low address in two parts
-
-    # send high address in two parts
-
-    # receive data until we are done
+        # write output to a file
+        device.write_output_file(save_file, verbose)
 
 
 if __name__ == "__main__":
@@ -32,29 +32,5 @@ if __name__ == "__main__":
     rc = RetCom87(port=PORT)
     rc.start_sequence()
 
-    reader = fdpexpect.fdspawn(rc)
-    reader.send(b'd')
-    state = reader.expect(b'BB:AAAA')
-    reader.send(b'00')
-    reader.expect(b':')
-    reader.send(b'0200')
-    reader.expect(b'BB:AAAA')
-    reader.send(b'00')
-    reader.expect(b':')
-    reader.send(b'0400')
-    time.sleep(1)
-
-    max_bytes = 256
-    fh = open("test.txt", "wb")
-
-    while rc.in_waiting > 0:
-      bytes_to_read = rc.in_waiting
-      bytes_to_read = max_bytes if bytes_to_read > max_bytes else bytes_to_read
-      data = rc.read(bytes_to_read)
-      print(f"Read {len(data)} bytes")
-      print(data)
-      fh.write(data)
-      time.sleep(0.1)
-
-    fh.close()
-    print()
+    memory_dump(rc, "00:E000", "00:FFFF", save_file="monitor.txt", verbose=True)
+    # memory_dump(rc, "00:0200", "00:1000", verbose=True)
